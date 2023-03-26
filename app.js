40729cc0e4d1606fdb8544e8aa1cc84ca7b4b2cb
sabujmahonto <sabujmahonto@gmail.com>
2023-03-26 21:01:22 +0600

    decilar a object and store a restaurant details

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 "use street"
+const restaurant ={
+    restaurantName : "Burgers house240",
+    location  : "Puran Dhaka, Dhaka , Bangladesh",
+    categories : ["Italian", "Germany", "Organic","local"],
+    startMenu : [
+        "Chicken cheese", 
+        "Matron Burger",
+        "Garlic Bread",
+        "Italian Pacy Pasta",
+        "Shahi Berayani",
+    ],
+    mainMenu : [
+        "Burger",
+        "Pizza",
+        "Chicken Kabub",
+        "Fajita",
+        "French fries"
+    ]
 
 
+
+}
