394984eb6956aa10ba37f82d91194fe738754bd9
sabujmahonto <sabujmahonto@gmail.com>
2023-03-27 05:27:51 +0600

    Array destructuring done

--- app.js
+++ app.js
@@ -16,8 +16,39 @@ const restaurant ={
         "Chicken Kabub",
         "Fajita",
         "French fries"
-    ]
+    ],
+    openingHours:{
+        sun:{
+            open:10,
+            close:21,
+        },
+        fri:{
+            open:9,
+            close:23,
+        },
+        wed:{
+            open:11,
+            close:22,
+        },
 
-
+    }
 
 }
+
+//Destructuring Array
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+
+
+const [x,y,z] = arr;
+console.log(x,y,z);
+
+const [main, , , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+const [food1, , , food2]= restaurant.mainMenu;
+
+console.log(food1, food2);
