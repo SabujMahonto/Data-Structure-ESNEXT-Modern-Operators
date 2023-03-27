77a2069126b8ab4ae4b67224c6e954960a11cc11
sabujmahonto <sabujmahonto@gmail.com>
2023-03-27 13:05:11 +0600

    Object destructuring done

--- app.js
+++ app.js
@@ -37,50 +37,61 @@ const restaurant ={
 
 }
 
-//Destructuring Array
-const arr = [1, 2, 3];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-console.log(a, b, c);
+// //Destructuring Array
+// const arr = [1, 2, 3];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// console.log(a, b, c);
 
 
-const [x,y,z] = arr;
-console.log(x,y,z);
+// const [x,y,z] = arr;
+// console.log(x,y,z);
 
-let [main, , , secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, , , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-const [food1, , , food2]= restaurant.mainMenu;
+// const [food1, , , food2]= restaurant.mainMenu;
 
-console.log(food1, food2);
+// console.log(food1, food2);
 
-// switching variable
-let first = 20;
-let second = 30;
-// let temp = first;
-// first = second;
-// second =temp;
-[first,second] = [second, first];
-console.log(first, second);
+// // switching variable
+// let first = 20;
+// let second = 30;
+// // let temp = first;
+// // first = second;
+// // second =temp;
+// [first,second] = [second, first];
+// console.log(first, second);
 
 
-[main, secondary] = [secondary, main]
+// [main, secondary] = [secondary, main]
 
-console.log(main, secondary);
+// console.log(main, secondary);
 
-    let [firstOrder, secondOrder] = restaurant.order(1,2)
+//     let [firstOrder, secondOrder] = restaurant.order(1,2)
 
    
-    console.log(firstOrder, secondOrder);
+//     console.log(firstOrder, secondOrder);
 
-    // Nested Destructuring
-    let friendsList = ['sabuj', 'sakib' , ['ripon' , 'rakib '], 'arpi']
+//     // Nested Destructuring
+//     let friendsList = ['sabuj', 'sakib' , ['ripon' , 'rakib '], 'arpi']
 
-    let [f1,f2,[f3,f4],f5] = friendsList;
-    console.log(f1,f2,f3,f4,f5);
+//     let [f1,f2,[f3,f4],f5] = friendsList;
+//     console.log(f1,f2,f3,f4,f5);
 
-    // default values
-   const newArr = [20, 40];
-   const [p, q, r=1, w=2] = newArr;
-   console.log(p, q, r, w);
+//     // default values
+//    const newArr = [20, 40];
+//    const [p, q, r=1, w=2] = newArr;
+//    console.log(p, q, r, w);
+
+
+// Destructuring Object
+
+const {restaurantName : name, startMenu: strMenu} = restaurant;
+
+console.log(name, strMenu);
+
+const {sun:sunday, fri:friday} = restaurant.openingHours;
+
+console.log(sunday, friday);
