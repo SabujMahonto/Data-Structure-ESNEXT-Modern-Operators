6f6ce22808678ec26c4dbdbb8a1759874a58bd9d
sabujmahonto <sabujmahonto@gmail.com>
2023-04-21 09:36:47 +0600

    reverse, concat, join method done

--- array.js
+++ array.js
@@ -14,3 +14,17 @@ const arr2 = ["j", "k", "l", "m", "n", "o"];
 
 console.log(arr2.splice(2,5));
 console.log(arr2);
+
+//reverse
+const arr4 = [22, 44,12, 43,99,09,33,];
+console.log(arr4.reverse());
+
+const arr3 =[33,22,66,44,33,77];
+// concat
+const joinArr = arr3.concat(arr3)
+console.log(joinArr);
+console.log([...arr3, ...arr4]);
+// join 
+const words = ["this", "is", "a", "nice", "day"];
+console.log(words.join(" "));
+
